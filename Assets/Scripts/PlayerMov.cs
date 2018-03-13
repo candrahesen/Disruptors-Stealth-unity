25c328351dc6899c055d82776ffea7fead9a72ce
candrahesen <candrahesen@gmail.com>
2018-03-13 18:06:09 +0700

    add character animation for idle, walk, and run

---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour {
+
+    public Transform playertrn;
+    public Rigidbody playerRB;
+    public int speed;
+    private bool isFalling = false;
+    public Vector3 JumpHeight = new Vector3(0, 5, 0);
+
+	void Start () {
+        playertrn = GetComponent<Transform>();
+        playerRB = GetComponent<Rigidbody>();
+	}
+
+
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            playertrn.Translate(new Vector3(speed, 0, 0) * speed * Time.deltaTime);
+        }           
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            playertrn.Translate(new Vector3(-speed, 0, 0) * speed * Time.deltaTime);
+        }            
+
+        else if (Input.GetKey(KeyCode.Space)) {
+            playerRB.velocity = JumpHeight;
+            isFalling = true;
+        }
+	}
+
+    void OnCollisionStay () {
+        isFalling = false;
+    }
+}
